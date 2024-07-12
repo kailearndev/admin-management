d989726168bdf5cd8562dd0fc3872f9227737b94
BM Kai <bm.hcmc.kai@gmail.com>
2024-07-12 18:23:44 +0700

    init

---
+++ src/components/menu/data.ts
@@ -0,0 +1,117 @@
+// import toast from 'react-hot-toast';
+import {
+  HiOutlineHome,
+  HiOutlineUser,
+  HiOutlineUsers,
+  HiOutlineCube,
+  HiOutlineClipboardDocumentList,
+  HiOutlineDocumentChartBar,
+  HiOutlinePencilSquare,
+  HiOutlineCalendarDays,
+  HiOutlinePresentationChartBar,
+  HiOutlineDocumentText,
+  HiOutlineArrowLeftOnRectangle,
+} from 'react-icons/hi2';
+// import { IoSettingsOutline } from 'react-icons/io5';
+
+export const menu = [
+  {
+    catalog: 'main',
+    listItems: [
+      // {
+      //   isLink: true,
+      //   url: '/',
+      //   icon: HiOutlineHome,
+      //   label: 'homepage',
+      // },
+      {
+        isLink: true,
+        url: '/',
+        icon: HiOutlineUser,
+        label: 'About Me',
+      },
+      {
+        isLink: true,
+        url: '/activities',
+        icon: HiOutlineUsers,
+        label: 'Activities',
+      },
+      {
+        isLink: true,
+        url: '/education',
+        icon: HiOutlineCube,
+        label: 'Education',
+      },
+    ],
+    // },
+    // {
+    //   catalog: 'lists',
+    //   listItems: [
+
+    //     {
+    //       isLink: true,
+    //       url: '/orders',
+    //       icon: HiOutlineClipboardDocumentList,
+    //       label: 'orders',
+    //     },
+    //     {
+    //       isLink: true,
+    //       url: '/posts',
+    //       icon: HiOutlineDocumentChartBar,
+    //       label: 'posts',
+    //     },
+    //   ],
+    // },
+    // {
+    //   catalog: 'general',
+    //   listItems: [
+    //     {
+    //       isLink: true,
+    //       url: '/notes',
+    //       icon: HiOutlinePencilSquare,
+    //       label: 'notes',
+    //     },
+    //     {
+    //       isLink: true,
+    //       url: '/calendar',
+    //       icon: HiOutlineCalendarDays,
+    //       label: 'calendar',
+    //     },
+    //   ],
+    // },
+    // {
+    //   catalog: 'analytics',
+    //   listItems: [
+    //     {
+    //       isLink: true,
+    //       url: '/charts',
+    //       icon: HiOutlinePresentationChartBar,
+    //       label: 'charts',
+    //     },
+    //     {
+    //       isLink: true,
+    //       url: '/logs',
+    //       icon: HiOutlineDocumentText,
+    //       label: 'logs',
+    //     },
+    //   ],
+    // },
+    // {
+    //   catalog: 'miscellaneous',
+    //   listItems: [
+    //     // {
+    //     //   isLink: true,
+    //     //   url: '/settings',
+    //     //   icon: IoSettingsOutline,
+    //     //   label: 'settings',
+    //     // },
+    //     {
+    //       isLink: true,
+    //       url: '/login',
+    //       icon: HiOutlineArrowLeftOnRectangle,
+    //       label: 'log out',
+    //     },
+    //   ],
+    // },
+  }
+];
